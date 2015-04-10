f483a423ab02bd074728b36053e68b9cd2286d58
Felicia Wold <feliciawold@gmail.com>
2015-04-10 16:24:48 -0600

    Added anjular files

---
+++ backend/apps/recipes/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'mosassy'
+from django.conf.urls import patterns, url
+from views import RecipeList
+urlpatterns = [
+     url('^recipes/$', RecipeList.as_view(), name='recipe-list'),
+
+]
+
