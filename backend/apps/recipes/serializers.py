316a89bd9427855587d95108fba4ac8a31ffc0ae
Felicia Wold <feliciawold@gmail.com>
2015-04-21 13:34:46 -0600

    added recipe ingredient delete tags

--- backend/apps/recipes/serializers.py
+++ backend/apps/recipes/serializers.py
@@ -13,5 +13,27 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
 
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
 
+        for ingredient in ingredients_data:
+            ingredient, created = Ingredient.objects.get_or_create(name=ingredient['name'])
+            recipe.ingredients.add(ingredient)
+        return recipe
+    def update(self, instance, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.desription = validated_data.get('description', instance.description)
+        instance.directions = validated_data.get('directions', instance.directions)
+
+        ingredients_list = []
+
+        for ingredient in ingredients_data:
+            ingredient, created = Ingredient.objects.get_or_create(name=ingredient["name"])
+            ingredients_list.append(ingredient)
+        instance.ingredients = ingredients_list
+        instance.save()
+        return instance
 
